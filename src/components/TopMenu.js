cf28395319581de304701a28712f363ddd24bdfb
Venkatesh Gopalakrishnan <96gvenkatesh@gmail.com>
2019-09-16 20:11:48 +0530

    added logo brand

--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -5,7 +5,19 @@ import { NavLink } from "react-router-dom";
 const TopMenu = () => {
   return (
     <Navbar expand="lg" variant="dark">
-      <Navbar.Brand href="/">Venkatesh G</Navbar.Brand>
+      <Navbar.Brand href="/">
+        <img
+          src="../gv_logo.png"
+          style={{
+            width: "15%",
+            borderBottomRightRadius: "40%",
+            borderTopLeftRadius: "40%",
+            borderBottomLeftRadius: "20%",
+            borderTopRightRadius: "20%"
+          }}
+          alt="Venkatesh Gopalakrishnan"
+        />
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto" style={{ textColor: "white" }}>
